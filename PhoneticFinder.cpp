a13e5262b17bf5c46c47b220fc5bdd8b82ddc0c3
LevanaSciari <57485490+LevanaSciari@users.noreply.github.com>
2020-03-22 21:40:24 +0200

    Add files via upload

---
+++ PhoneticFinder.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+#include<stdexcept>
+#include <string>
+
+#include <algorithm>
+#include"PhoneticFinder.hpp"
+
+using namespace std;
+
+
+    string phonetic::find(string text,string word){
+
+	
+       return word;
+    }
